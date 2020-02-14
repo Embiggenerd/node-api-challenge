ab3e3789f791faf0403b7b9e184dbe9eb2c89a5a
igor <embiggenerd@gmail.com>
2020-02-14 13:42:26 -0500

    client displaying actoins of project on click and goes back

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ const axios = require('axios')
 
 function App() {
   const [projects, setProjects] = useState([])
+  const [actions, setActions] = useState([])
 
   useEffect(() => {
     const getProjects = async () => {
@@ -14,8 +15,13 @@ function App() {
     getProjects()
   }, [])
 
-  const handleOnClickProject = () => {
+  const handleOnClickProject = async (projectID) => {
+    const { data } = await axios.get(`/projects/${projectID}/actions`)
+    setActions(data)
+  }
 
+  const handleOnClickAction = () => {
+    setActions([])
   }
 
   return (
@@ -23,12 +29,21 @@ function App() {
       <header className="App-header">
         <div className="projects-list">
           {projects.map(p =>
-            <div key={p.id}className="projects">
-              <h3>{p.name}</h3>
+            <div onClick={() => handleOnClickProject(p.id)} key={p.id} className="projects">
+              <h3 >{p.name}</h3>
               <p>{p.description}</p>
               <p> {p.completed ? 'completed' : 'incomplete'}</p>
             </div>)}
         </div>
+        {actions.length > 0 ? 
+          <div className="actions-list">
+          <button onClick={handleOnClickAction}>Go Back</button>
+            {actions.map(a => <div className="action">
+              <h4>{a.description}</h4>
+              <p>{a.notes}</p>
+              
+            </div>)}
+          </div> : null}
       </header>
     </div>
   );
